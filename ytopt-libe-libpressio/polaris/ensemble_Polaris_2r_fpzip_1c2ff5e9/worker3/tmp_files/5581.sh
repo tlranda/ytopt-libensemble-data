73f727ee2cd0c83bc933dec59ee0f8e267012106
Tom Randall <tlranda@clemson.edu>
2023-09-11 20:11:04 +0000

    A bunch of new data (zfpgpu, mgardgpu, fpzip, blosc-9)

---
+++ ytopt-libe-libpressio/polaris/ensemble_Polaris_2r_fpzip_1c2ff5e9/worker3/tmp_files/5581.sh
@@ -0,0 +1,11 @@
+{
+  "pressio:compressor": "roibin",
+  "roibin:background": "binning",
+  "roibin:roi": "fpzip",
+  "roibin:roi_size": [8,8,0],
+  "roibin:nthreads": {"type":0, "value": 1},
+  "binning:compressor": "fpzip",
+  "binning:shape": [2,2,1],
+  "binning:nthreads": {"type":0, "value": 1},
+  "fpzip:prec": {"type": 1, "value": 0}
+}
