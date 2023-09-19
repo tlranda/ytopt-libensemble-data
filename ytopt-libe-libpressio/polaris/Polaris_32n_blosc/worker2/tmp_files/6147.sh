0c1dd8b9067ad83894cc801436880be406b12b80
Tom Randall <tlranda@clemson.edu>
2023-09-19 20:10:59 +0000

    More data for LibPressio

---
+++ ytopt-libe-libpressio/polaris/Polaris_32n_blosc/worker2/tmp_files/6147.sh
@@ -0,0 +1,14 @@
+{
+  "pressio:compressor": "roibin",
+  "roibin:background": "binning",
+  "roibin:roi": "blosc",
+  "roibin:roi_size": [8,8,0],
+  "roibin:nthreads": {"type":0, "value": 1},
+  "binning:compressor": "blosc",
+  "binning:shape": [2,2,1],
+  "binning:nthreads": {"type":0, "value": 2},
+  "blosc:compressor": "zstd",
+  "blosc:doshuffle": {"type":1,"value":1},
+  "blosc:clevel": {"type":1,"value":3},
+  "blosc:numinternalthreads": {"type":1, "value": 3}
+}
